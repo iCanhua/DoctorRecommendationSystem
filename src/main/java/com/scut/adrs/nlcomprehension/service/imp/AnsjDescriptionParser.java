9b48654335f6d13a1f9b5b404f448b1718f78414
林博俊 <vincent_lbj@163.com>
2017-02-22 21:33:20 +0800

    过滤停用词

--- src/main/java/com/scut/adrs/nlcomprehension/service/imp/AnsjDescriptionParser.java
+++ src/main/java/com/scut/adrs/nlcomprehension/service/imp/AnsjDescriptionParser.java
@@ -1,37 +1,60 @@
 package com.scut.adrs.nlcomprehension.service.imp;
-import com.github.andrewoma.dexx.collection.ArrayList;
-import com.scut.adrs.nlcomprehension.service.*;
-import com.scut.adrs.util.DicInsertUtil;
 
-import java.util.List;
+import java.io.FileNotFoundException;
 
 import org.ansj.domain.Result;
-import org.ansj.domain.Term;
-import org.ansj.library.*;
-import org.ansj.recognition.Recognition;
+import org.ansj.recognition.impl.StopRecognition;
 import org.ansj.splitWord.analysis.DicAnalysis;
-import org.ansj.splitWord.analysis.ToAnalysis;
-import org.ansj.*;
 import org.springframework.stereotype.Service;
 
+import com.scut.adrs.nlcomprehension.service.DescriptionParser;
+import com.scut.adrs.util.DicInsertUtil;
+import com.scut.adrs.util.FileUtil;
+
 @Service
-public class AnsjDescriptionParser implements DescriptionParser{
-	
-	public AnsjDescriptionParser(){
-		userLibraryInit();
+public class AnsjDescriptionParser implements DescriptionParser {
+
+	// 停用词过滤
+	StopRecognition stopRecongnition;
+
+	public AnsjDescriptionParser() {
+		init();
 	}
-	
+
+	public void init() {
+		userLibraryInit();
+		stopWordLibraryInit();
+	}
+
 	@Override
 	public Result parse(String description) {
-		Result result= DicAnalysis.parse(description);
+		Result result = DicAnalysis.parse(description);
 		return result;
 	}
-	
-	private boolean userLibraryInit(){
+
+	private boolean userLibraryInit() {
 		DicInsertUtil dicInsertUtil;
 		return true;
 	}
-	
-	private void stopWordLibraryInit(){ 	
+
+	private void stopWordLibraryInit() {
+		stopRecongnition = new StopRecognition();
+		String[] stopWords = null;
+		try {
+			stopWords = FileUtil.readFileByLines("classpath:stopwords.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("读取文件出错");
+			e.printStackTrace();
+		}
+		stopRecongnition.insertStopWords(stopWords);
 	}
+
+	public StopRecognition getStopRecongnition() {
+		return stopRecongnition;
+	}
+
+	public void setStopRecongnition(StopRecognition stopRecongnition) {
+		this.stopRecongnition = stopRecongnition;
+	}
+
 }
