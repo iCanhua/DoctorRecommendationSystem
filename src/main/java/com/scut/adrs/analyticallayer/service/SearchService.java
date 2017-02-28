d1b549a2150292b0ae6450656b15c07160a30e48
林博俊 <vincent_lbj@163.com>
2017-02-28 18:49:26 +0800

    问答基本完成

--- src/main/java/com/scut/adrs/analyticallayer/service/SearchService.java
+++ src/main/java/com/scut/adrs/analyticallayer/service/SearchService.java
@@ -1,9 +1,12 @@
 package com.scut.adrs.analyticallayer.service;
 
 import com.scut.adrs.domain.Patient;
+import com.scut.adrs.nlcomprehension.InterConceptQuestion;
 
 public interface SearchService {
 
-	public Patient buildPatient(String[] symptoms, String[] bodySigns, String[] pathogeny, String[] medicalHistory)
-			throws Exception;
+	public Patient buildPatient(String[] symptoms, String[] bodySigns, String[] pathogeny, String[] medicalHistory,
+			Patient patient) throws Exception;
+
+	public Patient buildPatient(InterConceptQuestion interConceptQuestion, Patient patient);
 }
